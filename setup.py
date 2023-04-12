130cab71b3681662ec488024bea5d9ebac4eb8b4
Subhranil Sarkar <subhranil2605@gmail.com>
2023-04-12 19:53:26 +0530

    Add new code

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
+from typing import List
 
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-HYPHEN_E_DOT: str = "-e ."
-
-def get_rquirements(file_path: str) -> list[str]:
-    """
-    This function will return the list of requirements
-    """
-    requirements = []
-    with open(file_path) as f_obj:
-        requirements = f_obj.readlines()
-        requirements = [req.strip() for req in requirements]
-
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
-
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='Subhranil',
-    author_email='subhranil.sarkar2605@gmail.com',
-    packages=find_packages(),
-    install_requires=get_rquirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import numpy as np 
 import pandas as pd
 import dill
 import pickle
@@ -9,34 +10,36 @@ from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
 
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, "wb") as f_obj:
-            pickle.dump(obj, f_obj)
-        
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
-
-
-def evaluate_model(X_train, y_train, X_test, y_test, models, params, cv=3, n_jobs=3, verbose=1, refit=False):
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para = params[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model, para, cv=cv, n_jobs=n_jobs, verbose=verbose, refit=False)
-            gs.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train, y_train)  # train the model
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
@@ -46,14 +49,14 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, params, cv=3, n_job
             report[list(models.keys())[i]] = test_model_score
 
         return report
-            
+
     except Exception as e:
         raise CustomException(e, sys)
-
-
+    
 def load_object(file_path):
     try:
-        with open(file_path, "rb") as f_obj:
-            return pickle.load(f_obj)
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
